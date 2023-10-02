2db8e625aa05dab63f2acda22e3ff379b38f79ca
vmomenv <vmomenv@gmail.com>
2023-10-02 18:04:16 +0800

    header部分完善

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,24 +1,36 @@
 <template>
     <div>
         <el-container>
+          <el-aside width="200px">
             <common-aside/>
+          </el-aside>
             <el-container>
-              <el-header>Header</el-header>
+              <el-header>
+                <common-header/>
+              </el-header>
               <el-main>
                 <router-view></router-view>
               </el-main>
             </el-container>
           </el-container>
     </div>
-</template>
+    </template>
 <script>
-import CommonAside from '@/components/CommonAside.vue';
+import CommonAside from '../components/CommonAside.vue';
+import CommonHeader from '../components/CommonHeader.vue'
 export default {
     data(){
         return{}
     },
     components:{
-        CommonAside
+        CommonAside,
+        CommonHeader
     }
 }
 </script>
+<style>
+.el-header{
+  padding: 0;
+}
+
+</style>
