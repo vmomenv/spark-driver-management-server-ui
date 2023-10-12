9b855379fbb92c73760aebc469f7900489a79370
vmomenv <vmomenv@gmail.com>
2023-10-12 16:45:16 +0800

    更新面包屑

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="head-container">
         <div class="l-content">
-            <el-button @click="handleMenu" icon="el-icon-menu" size="mini"></el-button>
+            <el-button style="margin-right:20px"  @click="handleMenu" icon="el-icon-menu" size="mini"></el-button>
             <!-- 面包屑 -->
             <el-breadcrumb separator="/">
                 <el-breadcrumb-item v-for="item in tags" :key="item.path" :to="{ path: item.path }">{{ item.label }}</el-breadcrumb-item>
@@ -64,7 +64,24 @@ export default {
             border-radius:50%;
         }
     }
-
+    .l-content{
+        display: flex;
+        align-items: center;
+        /deep/.el-breadcrumb__item{
+            .el-breadcrumb__inner{
+                font-weight: normal;
+                &.is-link{
+                    color:#666
+                }
+            }
+            &:last-child{
+                .el-breadcrumb__inner{
+                    color:#fff;
+                }
+            }
+        }
+    }
+    
 }
 
 </style>
