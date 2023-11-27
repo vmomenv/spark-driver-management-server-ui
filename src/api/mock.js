7d372776604b38dc8318cb22d6bb6151467f07b2
vmomenv <vmomenv@gmail.com>
2023-11-27 22:54:05 +0800

    搜索用户页完成

--- src/api/mock.js
+++ src/api/mock.js
@@ -9,6 +9,6 @@ Mock.mock('/api/home/getData', homeApi.getStatisticalData)
 Mock.mock('/api/user/add', 'post', user.createUser)
 Mock.mock('/api/user/edit', 'post', user.updateUser)
 Mock.mock('/api/user/del', 'post', user.deleteUser)
-Mock.mock('/api/user/getUser', user.getUserList)
+Mock.mock(/api\/user\/getUser/, user.getUserList)
 
 // Mock.mock(/api\/permission\/getMenu/, 'post',permission.getMenu)
--- src/views/User.vue
+++ src/views/User.vue
@@ -14,15 +14,16 @@
           </el-form-item>
           <el-form-item label="性别" prop="sex">
             <el-select v-model="form.sex" placeholder="请选择">
-              <el-option label="男" value="1"></el-option>
-              <el-option label="女" value="0"></el-option>
+              <el-option label="男" :value="1"></el-option>
+              <el-option label="女" :value="0"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="出生日期" prop="birth">
             <el-date-picker
               v-model="form.birth"
               type="date"
-              placeholder="选择日期"></el-date-picker>
+              placeholder="选择日期"
+              value-format="yyyy-MM-DD"></el-date-picker>
           </el-form-item>
           <el-form-item label="地址" prop="addr">
             <el-input placeholder="请输入地址" v-model="form.addr"></el-input>
@@ -40,47 +41,65 @@
         <el-button @click="handleAdd" type="primary">
             + 新增
         </el-button> 
-          <el-table
-              :data="tableData"
-              style="width: 100%">
-              <el-table-column
-                prop="name"
-                label="姓名">
-              </el-table-column>
-              <el-table-column
-                prop="sexLabel"
-                label="性别">
-                <template slot-scope="scope">
-                  <span>{{ scope.row.sex ==1 ? '男':'女'}}</span>
-                </template>
-              </el-table-column>
-              <el-table-column
-                prop="age"
-                label="年龄">
-              </el-table-column>
-              <el-table-column
-                prop="birth"
-                label="出生日期">
-              </el-table-column>
-              <el-table-column
-                prop="addr"
-                label="地址">
-              </el-table-column>
-              <el-table-column
-                prop="addr"
-                label="地址">
-                <template slot-scope="scope">
-                  <el-button size="mini" @click="handleEdit(scope.row)">编辑</el-button>
-                  <el-button type="danger" size="mini" @click="handleDelete(scope.row)">删除</el-button>
-                </template>
-            </el-table-column>
-
-        </el-table>
+        <!-- form搜索区域 -->
+        <el-form :inline="true" :model="userForm">
+          <el-form-item>
+            <el-input placeholder="请输入名称" v-model="userForm.name"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">搜索</el-button>
+          </el-form-item>
+        </el-form>
       </div>
+      <div class="common-table">      
+        <el-table
+        stripe
+        height="90%"
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          prop="name"
+          label="姓名">
+        </el-table-column>
+        <el-table-column
+          prop="sexLabel"
+          label="性别">
+          <template slot-scope="scope">
+            <span>{{ scope.row.sex ==1 ? '男':'女'}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="age"
+          label="年龄">
+        </el-table-column>
+        <el-table-column
+          prop="birth"
+          label="出生日期">
+        </el-table-column>
+        <el-table-column
+          prop="addr"
+          label="地址">
+        </el-table-column>
+        <el-table-column
+          prop="addr"
+          label="地址">
+          <template slot-scope="scope">
+            <el-button size="mini" @click="handleEdit(scope.row)">编辑</el-button>
+            <el-button type="danger" size="mini" @click="handleDelete(scope.row)">删除</el-button>
+          </template>
+      </el-table-column>
+    </el-table>
+      <div class="pager">
+          <el-pagination
+            layout="prev, pager, next"
+            :total="total"
+            @current-change="handlePage">
+          </el-pagination>
+      </div></div>
     </div>
 </template>
 <script>
-import {getUser,addUser,editUser} from '../api'
+import {getUser,addUser,editUser,delUser} from '../api'
 export default {
     data(){
         return{
@@ -102,7 +121,15 @@ export default {
             tableData:[
 
             ],
-            modalType:0 //0表示新增的弹窗，1表示编辑的弹窗 
+            modalType:0 ,//0表示新增的弹窗，1表示编辑的弹窗 
+            total:0,//当前总条数
+            pageData:{
+              page:1,
+              limit:10
+            },
+            userForm:{
+              name:''
+            }
               
         }
     },
@@ -125,7 +152,7 @@ export default {
             }
             
             //表单数据
-            console .log(this.form,"form")
+            console.log(this.form,"form")
 
             //重置表单
             this.$refs.form.resetFields()
@@ -145,9 +172,29 @@ export default {
       },
       handleEdit(row){
         this.modalType=1
+        this.dialogVisible=true
+        //需要注意对当前行数据进行深拷贝，否则会出错
+        this.form=JSON.parse(JSON.stringify(row))
       },
       handleDelete(row){
-
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          delUser({id:row.id}).then(()=>{
+            this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+          this.getList()
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
       },
       handleAdd(){
         this.modalType=0
@@ -155,10 +202,22 @@ export default {
       },
       getList(){
           //获取的列表数据
-          getUser().then(({data})=>{
+          getUser({params:{...this.userForm,...this.pageData}}).then(({data})=>{
             console.log(data)
             this.tableData=data.list
+            this.total=data.count ||0
+
           })
+      },
+      handlePage(val){
+        //选择页面的回调函数
+        // console.log(val,'val')
+        this.pageData.page=val
+        this.getList()
+      },
+      //列表的查询条件
+      onSubmit(){
+        this.getList()
       }
     },
     mounted(){
@@ -166,38 +225,23 @@ export default {
     }
 }
 </script>
-
-<!-- {
-  model:'name',
-  label:'姓名',
-  type:'input'
-},
-{
-  model:'age',
-  label:'年龄',
-  type:'input'
-},
-{
-  model:'sex',
-  label:'性别',
-  type:'select',
-  opts:[
-      {
-      label:'男',
-      value:1
-    },{
-      label:'女',
-      value:0
+<style lang="less" scoped>
+.manage{
+  height: 90%;
+  .manage-header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .common-table{
+    position: relative;
+    height: calc(100% - 62px);
+    .pager{
+      position:absolute;
+      bottom: 0;
+      right:20px;
     }
-  ]
-},
-{
-  model:'birth',
-  label:'出生日期',
-  type:'date'
-},
-{
-  model:'addr',
-  label:'地址',
-  type:'input'
-} -->
+  }
+}
+</style>
+
